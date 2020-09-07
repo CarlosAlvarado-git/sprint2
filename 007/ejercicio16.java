ed015ec2cb001b597a48767ea6a5bd8acb4a5728
Sophia Gamarro <lsophiagr@gmail.com>
2020-09-07 02:53:02 -0600

    update ejercicios 7-16

---
+++ 007/ejercicio16.java
@@ -0,0 +1,15 @@
+public class ejercicio16 {
+    
+    public static void main(String[] args) {
+    	for(int i = 1; i<=10; i++){
+    		for (int j=1; j<=10; j++ ) {
+    			System.out.printf("%-2d * %-2d = %-3d |", j, i, (i*j));
+    			
+    		}
+
+    		System.out.println();
+    	}
+
+ }
+ 
+}
